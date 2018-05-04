9a9ab264cf1c8c66dcc37bba96989c62023d17e4
shixiaoying <137270366@qq.com>
2018-05-04 15:21:11 +0800

    修改样式

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('default', ['browser-sync']);
 // Static server
 gulp.task('browser-sync', function() {
-	var files = ['*.html', '**/**/*.js', '**/*.js', 'css/*.css', '**/**/*.html']
+	var files = ['*.html', '**/**/*.js', '**/*.js', '*/*.css', '**/**/*.html']
     browserSync.init(files, {
         server: {
             baseDir: "./"
